e4d2f4b5c4f5ba1d696179731b89f05d4e5d0b28
reezop24 <rezashahrill@icloud.com>
2026-02-10 05:47:45 +0800

    Create presets.js

---
+++ presets.js
@@ -0,0 +1,32 @@
+// ===============================
+// PRESET DATA HEMSS
+// ===============================
+
+// SENARAI KELAS
+const PRESET_KELAS = [
+  "Seroja",
+  "Mawar",
+  "Melati",
+  "Teratai",
+  "Kenanga",
+  "Cempaka",
+  "Anggerik",
+  "Orkid",
+  "Bakawali",
+  "Ros"
+];
+
+// KEBERADAAN GURU
+const PRESET_KEBERADAAN = [
+  "CR",
+  "CTR",
+  "CRK",
+  "MC",
+  "Kuarantin",
+  "Bengkel",
+  "Mesyuarat",
+  "Taklimat",
+  "Temujanji",
+  "Rawatan",
+  "Lain-lain"
+];
