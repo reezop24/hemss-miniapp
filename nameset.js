286fba6b5dfc2923ea26a631888ca594427b0999
reezop24 <rezashahrill@icloud.com>
2026-02-10 08:48:17 +0800

    Create nameset.js

---
+++ nameset.js
@@ -0,0 +1,36 @@
+<script>
+/* =========================
+   GLOBAL NAME SET
+   ========================= */
+
+const NAMESET = [
+  "Ahmad Zaki",
+  "Siti Aisyah",
+  "Norhafizah",
+  "Muhammad Aiman",
+  "Nur Syuhada",
+  "Faizal Hakim",
+  "Nurul Iman",
+  "Hafiz Rahman",
+  "Aina Sofia",
+  "Daniel Akmal"
+];
+
+// init search box
+function initNameSearch(input) {
+  const listId = "nameset-list";
+
+  if (!document.getElementById(listId)) {
+    const datalist = document.createElement("datalist");
+    datalist.id = listId;
+    NAMESET.forEach(name => {
+      const opt = document.createElement("option");
+      opt.value = name;
+      datalist.appendChild(opt);
+    });
+    document.body.appendChild(datalist);
+  }
+
+  input.setAttribute("list", listId);
+}
+</script>
